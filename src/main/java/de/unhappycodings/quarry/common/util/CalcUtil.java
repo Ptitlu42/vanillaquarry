5f4534647583e5f74fc501b7682decebb1777b9f
Ptitlu42 <lucas.beyer@gmx.fr>
2025-09-03 00:31:09 +0200

    perf(calc): reduce fuel consumption by factor of 10

--- src/main/java/de/unhappycodings/quarry/common/util/CalcUtil.java
+++ src/main/java/de/unhappycodings/quarry/common/util/CalcUtil.java
@@ -59,20 +59,24 @@ public class CalcUtil {
         int x2 = pos2.getX();
         int y2 = pos2.getY();
         int z2 = pos2.getZ();
-        if (y1 > 320) y1 = 320;
-        if (y1 < -64) y1 = -64;
-        if (y2 > 320) y2 = 230;
-        if (y2 < -64) y2 = -64;
+        if (y1 > 320)
+            y1 = 320;
+        if (y1 < -64)
+            y1 = -64;
+        if (y2 > 320)
+            y2 = 230;
+        if (y2 < -64)
+            y2 = -64;
         return Math.abs((x1 - x2 + 1) * (y1 - y2 + 1) * (z1 - z2 + 1));
     }
 
     public static float getNeededTicks(int mode, int speed) {
         int fuelModifier = switch (mode) {
-            case 1 -> CommonConfig.quarryEfficientModeConsumption.get();  // Efficient
-            case 2 -> CommonConfig.quarryFortuneModeConsumption.get();    // Fortune
-            case 3 -> CommonConfig.quarrySilkTouchModeConsumption.get();  // Silktouch
-            case 4 -> CommonConfig.quarryVoidModeConsumption.get();       // Void
-            default -> CommonConfig.quarryDefaultModeConsumption.get();   // Default
+            case 1 -> CommonConfig.quarryEfficientModeConsumption.get(); // Efficient
+            case 2 -> CommonConfig.quarryFortuneModeConsumption.get(); // Fortune
+            case 3 -> CommonConfig.quarrySilkTouchModeConsumption.get(); // Silktouch
+            case 4 -> CommonConfig.quarryVoidModeConsumption.get(); // Void
+            default -> CommonConfig.quarryDefaultModeConsumption.get(); // Default
         };
         switch (speed) {
             case 0 -> fuelModifier *= CommonConfig.quarrySpeedOneModifier.get();
@@ -80,7 +84,8 @@ public class CalcUtil {
             case 2 -> fuelModifier *= CommonConfig.quarrySpeedThreeModifier.get();
             case 3 -> fuelModifier *= CommonConfig.quarrySpeedFourModifier.get();
         }
-        return fuelModifier;
+        // Division par 10 pour refléter la réduction de consommation
+        return fuelModifier / 10f;
     }
 
 }
